e75383623a271bd649428a0a2d26c33e82eea093
Colby Janecka <colbyjanecka@aol.com>
2020-02-18 17:59:06 -0600

    created datastore for storing subscribed emails

---
+++ src/main/java/blogproject/SubscriptionDataServlet.java
@@ -0,0 +1,91 @@
+package blogproject;
+
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import com.google.appengine.api.users.*;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+
+public class SubscriptionDataServlet extends HttpServlet{
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	
+		throws IOException{
+		
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		
+		response.setContentType("text/html;");
+	    response.getOutputStream().println("<h1>Subscription Status</h1>");
+	    response.getOutputStream().println("<ul>");
+	    
+	    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+	    
+	    Query query = new Query("SubscribedUser");
+	    PreparedQuery results = datastore.prepare(query);
+	    
+	    for(Entity subscribedUserEntity : results.asIterable()) {
+	    	
+	    	String email = (String) subscribedUserEntity.getProperty("email");
+	    	Boolean isSubscribed = (Boolean) subscribedUserEntity.getProperty("subscribed");
+	    	
+	    	response.getOutputStream().println("<li>" + email + " - " + isSubscribed + "</li>");
+	    }
+	    
+	    response.getOutputStream().println("</ul>");
+	    response.getOutputStream().println("<p><a href=\"/subscribe\">Back</a></p>");
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws IOException {
+		
+		Boolean Status = Boolean.valueOf(request.getParameter("status"));
+		
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		String email = user.getEmail();
+		
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		
+		Query query = new Query("SubscribedUser");
+		
+		Key existingUserKey = KeyFactory.createKey("SubscribedUser", email);
+		
+		try {
+			
+			Entity existingUserEntity = datastore.get(existingUserKey);		
+			existingUserEntity.setProperty("subscribed", Status);
+			datastore.put(existingUserEntity);
+			
+			
+		} catch(EntityNotFoundException e) {
+			
+			System.out.println("User " + user.getEmail() + " is not in datastore. creating new entity.");
+			
+			Entity newUserEntity = new Entity("SubscribedUser", email);
+			newUserEntity.setProperty("email", email);
+
+			newUserEntity.setProperty("subscribed", Status);
+			datastore.put(newUserEntity);
+			
+		}
+		
+		System.out.println("User" + user.getEmail() + " wants to subscribe: " + Status);
+		
+		//datastore.put(subscribedUserEntity);
+		
+		response.sendRedirect("/subscriptionData");
+		
+	}
+}
---
+++ src/main/webapp/subscribe.jsp
@@ -0,0 +1,94 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="java.util.List" %>
+<%@ page import="com.google.appengine.api.users.User" %>
+<%@ page import="com.google.appengine.api.users.UserService" %>
+<%@ page import="com.google.appengine.api.users.UserServiceFactory" %>
+<%@ page import="com.google.appengine.api.datastore.DatastoreService" %>
+<%@ page import="com.google.appengine.api.datastore.DatastoreServiceFactory" %>
+<%@ page import="com.google.appengine.api.datastore.Query" %>
+<%@ page import="com.google.appengine.api.datastore.Entity" %>
+<%@ page import="com.google.appengine.api.datastore.FetchOptions" %>
+<%@ page import="com.google.appengine.api.datastore.Key" %>
+<%@ page import="com.google.appengine.api.datastore.KeyFactory" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+
+
+ 
+ <html>
+ 
+	<head>
+		<link type="text/css" rel="stylesheet" href = "/stylesheets/main.css" />
+	</head>
+ 	<body id="body">
+ 	 	
+ 		<%-- colby is now driving --%>
+ 			
+
+	 	<%
+	 	String blogName = request.getParameter("blogName");
+	 	if(blogName == null)
+	 	{
+	 		blogName = "main";
+	 	}
+	 	
+	 	pageContext.setAttribute("blogName", blogName);
+	 	
+	 	UserService userService = UserServiceFactory.getUserService();
+	    User user = userService.getCurrentUser();
+	    %>    
+	    
+	    <table style="width:100%" id="header">
+	    	<tr>
+	    	
+	    		<th style="width:70%" >
+	   				<p color="red">CC Blog</p>
+	   				<p>Welcome to our blog! Here you can post blogs and blog related things!</p>
+	    		</th>
+	    		
+	    		<th><img src="blog.jpg" alt="Blog Pic" width="100%" height="50%"></th>
+	   		 </tr>
+	    
+	    </table>
+	    
+	    
+	    <div class="cent">
+		    <%
+			if (user != null){
+			
+		    	pageContext.setAttribute("user", user);
+		    	%>
+		    	
+		    	<p>Your current email:  ${fn:escapeXml(user.email)}</p>
+		    	
+		    	<a class="button" href="<%= userService.createLogoutURL(request.getRequestURI()) %>">SignOut</a>
+				<br><br>
+		    	
+		    	<form method="POST" action="/subscriptionData">
+		    		<input type="radio" id="yes" name="status" value=true>
+		    		<label for="yes">Subscribe</label>
+		    		<input type="radio" id="no" name="status" value=false>
+		    		<label for="no">Unsubscribe</label>
+		    		<br>
+		    		<button>Submit</button>
+		    	</form>
+		    	
+				<%
+		    } else {
+				%>
+				
+				<p>You need to
+	
+				<a href="<%= userService.createLoginURL(request.getRequestURI()) %>">Sign in</a>
+	
+				in order to sign up for the subscription service!</p>
+				
+			<%
+		    } 
+		    %>
+		    
+	     
+	    </div>
+		
+ 	</body>
+ 
+ </html>
